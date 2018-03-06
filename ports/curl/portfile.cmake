252811bd81abe6172af398f8aef714f23dd3d038
Michał Janiszewski <janisozaur@users.noreply.github.com>
2018-03-06 15:28:47 +0100

    Update portfile.cmake

--- ports/curl/portfile.cmake
+++ ports/curl/portfile.cmake
@@ -12,7 +12,7 @@ vcpkg_apply_patches(
     PATCHES
         ${CMAKE_CURRENT_LIST_DIR}/0001_cmake.patch
         ${CMAKE_CURRENT_LIST_DIR}/0002_fix_uwp.patch
-        ${CMAKE_CURRENT_LIST_DIR}/0003_fix_libraries.patch
+        ${CMAKE_CURRENT_LIST_DIR}/0003-fix-arm.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" CURL_STATICLIB)
