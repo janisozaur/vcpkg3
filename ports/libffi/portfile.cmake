f83aba6a9ea5b92a33a5ab6d7c7a1c06aa5b9463
Michał Janiszewski <janisozaur+signed@gmail.com>
2019-11-10 21:43:28 +0100

    ffi

--- ports/libffi/portfile.cmake
+++ ports/libffi/portfile.cmake
@@ -1,19 +1,11 @@
 include(vcpkg_common_functions)
 
-if(NOT VCPKG_TARGET_ARCHITECTURE STREQUAL x86 AND NOT VCPKG_TARGET_ARCHITECTURE STREQUAL x64)
-    message(FATAL_ERROR "Architecture not supported")
-endif()
-
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libffi/libffi
-    REF v3.1
-    SHA512 b214e4a876995f44e0a93bad5bf1b3501ea1fbedafbf33ea600007bd08c9bc965a1f0dd90ea870281c3add6c051febd19aa6cdce36f3ee8ba535ba2c0703153c
+    REF v3.3-rc2
+    SHA512 3470b4f8dcb5529a6b8f8a6bcadb15df5732a973b2e18f016a922e0f17c4961cea6de46dae9eab0f75a9970921e5c23b310b90f4b12355e2ef84592ae1232a38
     HEAD_REF master
-    PATCHES
-        export-global-data.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
@@ -32,14 +24,15 @@ vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 vcpkg_fixup_cmake_targets()
 
+if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
 file(READ ${CURRENT_PACKAGES_DIR}/include/ffi.h FFI_H)
-string(REPLACE "/* *know* they are going to link with the static library. */"
-"/* *know* they are going to link with the static library. */
+string(REPLACE "   *know* they are going to link with the static library.  */"
+"   *know* they are going to link with the static library.  */
 
 #define FFI_BUILDING
-
 " FFI_H "${FFI_H}")
 file(WRITE ${CURRENT_PACKAGES_DIR}/include/ffi.h "${FFI_H}")
+endif()
 
 file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libffi)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/libffi/LICENSE ${CURRENT_PACKAGES_DIR}/share/libffi/copyright)
