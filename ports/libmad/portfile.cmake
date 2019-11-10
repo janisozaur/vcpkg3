42a5466cc7e730cf7269e9284833477aa4cf43cc
Michał Janiszewski <janisozaur+signed@gmail.com>
2019-11-10 22:55:02 +0100

    [libmad] Fix libmad header for non-x86 MSVC targets

--- ports/libmad/portfile.cmake
+++ ports/libmad/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_download_distfile(ARCHIVE
     URLS "http://download.sourceforge.net/mad/libmad-0.15.1b.tar.gz"
     FILENAME "libmad-0.15.1b.tar.gz"
     SHA512 2cad30347fb310dc605c46bacd9da117f447a5cabedd8fefdb24ab5de641429e5ec5ce8af7aefa6a75a3f545d3adfa255e3fa0a2d50971f76bc0c4fc0400cc45
+    PATCHES
+        0001-Fix-MSVC-ARM.patch
 )
 
 vcpkg_extract_source_archive_ex(
